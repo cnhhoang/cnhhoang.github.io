2213944a39820848c16d207ca2266369c9668eca
cnhhoang <cnhhoang.sec@gmail.com>
2024-09-01 02:36:57 +1000

    Add a flip-on-hover Promithic logo

---
+++ app/components/work-experience.tsx
@@ -0,0 +1,14 @@
+import PromithicLogo from "~/assets/promithic-logo";
+
+export default function WorkExperience()
+{
+    return (
+            <div className="relative h-screen w-full border">
+                <div className="h-fit w-fit hover:transform hover:scale-x-[-1] transition-transform duration-1000">
+                    <PromithicLogo/>
+                </div>
+            </div>
+
+         
+    );
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,6 +2,7 @@ import type { MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
 import { useOptionalUser } from "~/utils";
 import LandingPage from "~/components/landing-page";
+import WorkExperience from "~/components/work-experience";
 import Footer from "~/components/footer";
 // ====================================================================================================
 export const meta: MetaFunction = () => [{ title: "Hoang Chung" }];
@@ -12,6 +13,7 @@ export default function Index() {
     <div className={`min-h-screen bg-gradient-to-b from-sky-950 via-cyan-900 to-blue-950`}>
     {/* <div className={`min-h-screen bg-gradient-to-b from-slate-900 to-blue-800`}> */}
       <LandingPage/>
+      <WorkExperience/>
       <Footer/>
     </div>
   );
