494aef36b18a77147354db17370c9538149bcbc6
cnhhoang <cnhhoang.sec@gmail.com>
2024-09-01 17:41:38 +1000

    Integrate LandingPage to Index
    - Prepare for updating header, rendering child routes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,20 +1,52 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
+import { useState } from 'react';
+import Typewriter from '~/lib/typewriter';
 import { useOptionalUser } from "~/utils";
-import LandingPage from "~/components/landing-page";
-import WorkExperience from "~/components/work-experience";
-import Footer from "~/components/footer";
-// ====================================================================================================
+
+//====================================================================================================
 export const meta: MetaFunction = () => [{ title: "Hoang Chung" }];
 
+//====================================================================================================
 export default function Index() {
   const user = useOptionalUser();
+  const [active, setActive] = useState(0);
+  const careers = [
+      {
+        text: "programmer",
+        setting: "text-4xl text-green-600"
+      },
+      {
+        text: "software developer",
+        setting: "text-4xl text-sky-600"
+      },
+      {
+        text: "penetration tester",
+        setting: "text-4xl text-red-600"
+      },
+      {
+        text: "researcher",
+        setting: "text-4xl text-purple-500"
+      },
+  ];
+
+//****************************************************************************************************  
   return (
     // <div className={`min-h-screen bg-gradient-to-b from-sky-950 via-cyan-900 to-blue-950`}>
     <div className={`min-h-screen bg-gradient-to-b from-slate-900 to-blue-800`}>
-      <LandingPage/>
-      <WorkExperience/>
-      <Footer/>
+      <div className="h-screen w-full flex flex-col justify-center items-center focus:border-transparent select-none">
+        <div className = "grid grid-rows-2">
+            <div className="flex justify-center items-center">
+                <Typewriter text="Unlocking potential as a " textSetting="text-4xl text-white" id={0} nextId={1} setActive={setActive} active={active} />
+            </div>
+            <div className="flex justify-center items-center">
+                {
+                    careers.map((career, index) => (
+                        <Typewriter key={index} text={career.text} textSetting={career.setting} erase={true} id={index+1} nextId={index+1===careers.length ? 1 : index+2} setActive={setActive} active={active} />
+                    ))
+                }
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
