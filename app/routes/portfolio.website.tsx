73cba00d05b35f721e52e01b9c1f301a72c4c801
cnhhoang <cnhhoang.sec@gmail.com>
2024-09-27 23:54:43 +1000

    Update website purpose/description

--- app/routes/portfolio.website.tsx
+++ app/routes/portfolio.website.tsx
@@ -4,8 +4,10 @@
 export default function Website()
 {
     return (
-        <div className="h-full w-full flex items-center justify-center">
-            Website: Under development!
+        <div className="h-full w-full flex items-center justify-center p-10">
+            This website reflects my journey as a software engineer and penetration tester, offering insight into my work and growth within the tech industry.
+            Serving as an interactive resume, it highlights my professional achievements, skills, and the technologies I’ve mastered. 
+            Whether you’re a potential collaborator, recruiter, or fellow enthusiast, please feel free to reach out via email - I would be happy to connect.
         </div>
     );
 }
