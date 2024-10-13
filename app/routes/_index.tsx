cbab01d8d3e42160df392381a1ad59e589c6787f
cnhhoang <cnhhoang.main@gmail.com>
2024-10-13 15:36:11 +1100

    Multiple UI improvements
        Update sequential fade-in effect for Achievements
        Update description color for Education
        Update organization text color for Education
        Update effect for work-experience
        Update text size of index (About) page
        Update Page Loading text

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,19 +6,19 @@ import Typewriter from '~/lib/typewriter';
 const careers = [
   {
     text: "programmer",
-    setting: "text-4xl text-emerald-500"
+    setting: "text-6xl text-emerald-500"
   },
   {
     text: "software developer",
-    setting: "text-4xl text-sky-500"
+    setting: "text-6xl text-sky-500"
   },
   {
     text: "penetration tester",
-    setting: "text-4xl text-red-500"
+    setting: "text-6xl text-red-500"
   },
   {
     text: "researcher",
-    setting: "text-4xl text-violet-500"
+    setting: "text-6xl text-violet-500"
   },
 ];
 
@@ -28,11 +28,11 @@ export default function Index() {
 
   return (
     <div className="h-[calc(100vh-3rem)] w-full flex flex-col justify-center items-center focus:border-transparent select-none">
-      <div className = "grid grid-rows-2">
+      <div className = "grid grid-rows-3">
         <div className="flex justify-center items-center">
           <Typewriter text="Unlocking potential as a " textSetting="text-4xl text-white" id={0} nextId={1} setActive={setActive} active={active} />
         </div>
-        <div className="flex justify-center items-center">
+        <div className="row-span-2 flex justify-center items-center">
           {
               careers.map((career, index) => (
                   <Typewriter key={index} text={career.text} textSetting={career.setting} erase={true} id={index+1} nextId={index+1===careers.length ? 1 : index+2} setActive={setActive} active={active} />
