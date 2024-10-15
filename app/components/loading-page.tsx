4630ac6c079608e7421a3fd8f117e69de345a240
cnhhoang <cnhhoang.main@gmail.com>
2024-10-15 19:47:38 +1100

    Add LoadingPage component

---
+++ app/components/loading-page.tsx
@@ -0,0 +1,28 @@
+import { LoaderCircle } from "lucide-react";
+
+
+export default function LoadingPage()
+{
+    const skeletonColor = " bg-emerald-600 ";
+
+    return (
+        <div className="h-full w-full flex flex-col justify-center items-center space-y-6">
+            <div className={`text-6xl text-emerald-500 flex items-center justify-center`}>
+                <span>Page L</span>
+                <LoaderCircle className="h-10 w-10 relative top-[0.3rem] animate-spin" />
+                <span>ading...</span>
+            </div>
+
+            <div className="h-28 w-96 space-y-3">
+                <div className="h-fit w-full flex">
+                    <div className={`h-12 w-12 rounded-full ${skeletonColor} animate-pulse`} />
+                    <div className={`h-12 w-80 rounded-lg ml-auto ${skeletonColor} animate-pulse`}/>
+                </div>
+
+                <div className={`h-5 w-full rounded-md ${skeletonColor} animate-pulse`}/>
+                <div className={`h-5 w-full rounded-md ${skeletonColor} animate-pulse`}/>
+
+            </div>
+        </div>
+    );
+}
