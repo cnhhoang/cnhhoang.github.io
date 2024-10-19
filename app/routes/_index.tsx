b173cf798bf41efba576d274bad4ffd8c90bd1a3
cnhhoang <cnhhoang.main@gmail.com>
2024-10-20 01:17:37 +1100

    Add responsive design to Index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,22 +3,23 @@ import { useState } from 'react';
 import Typewriter from '~/lib/typewriter';
 
 //====================================================================================================
+const typeWriterSize = "text-4xl sm:text-5xl md:text-6xl ";
 const careers = [
   {
     text: "programmer",
-    setting: "text-6xl text-green-500"
+    setting: typeWriterSize + "text-green-500"
   },
   {
     text: "software developer",
-    setting: "text-6xl text-sky-600"
+    setting: typeWriterSize + "text-sky-600"
   },
   {
     text: "penetration tester",
-    setting: "text-6xl text-red-600"
+    setting: typeWriterSize + "text-red-600"
   },
   {
     text: "researcher",
-    setting: "text-6xl text-violet-700"
+    setting: typeWriterSize + "text-violet-700"
   },
 ];
 
@@ -30,7 +31,7 @@ export default function Index() {
     <div className="h-[calc(100vh-3rem)] w-full flex flex-col justify-center items-center focus:border-transparent select-none">
       <div className = "grid grid-rows-3">
         <div className="flex justify-center items-center">
-          <Typewriter text="Unlocking potential as a " textSetting="text-4xl text-white" id={0} nextId={1} setActive={setActive} active={active} />
+          <Typewriter text="Unlocking potential as a " textSetting="text-2xl sm:text-3xl md:text-4xl text-white" id={0} nextId={1} setActive={setActive} active={active} />
         </div>
         <div className="row-span-2 flex justify-center items-center">
           {
