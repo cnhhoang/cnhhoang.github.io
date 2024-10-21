a87747a244d44bd103f70b1692b681f58271d8ed
cnhhoang <cnhhoang.main@gmail.com>
2024-10-21 22:35:11 +1100

    Update md responsive to About

--- app/lib/terminal.tsx
+++ app/lib/terminal.tsx
@@ -22,22 +22,22 @@ const Terminal: React.FC<TerminalProps> = ({ children, className = "", disabled 
     <ScrollArea
       className={`relative text-white p-4 shadow-lg 
                 bg-gray-800 rounded-2xl  border-8 border-b-24 border-black
-                sm:bg-gray-900 sm:rounded-md sm:border sm:border-gray-700 ${className}`}
+                lg:bg-gray-900 lg:rounded-md lg:border lg:border-gray-700 ${className}`}
       style={{ fontFamily: "monospace" }}
     >
       {/* Terminal header */}
       <div className="flex flex-col">
-        <div className="hidden sm:flex absolute z-10 bg-gray-900 w-full space-x-2 pb-2 mb-2 border-b border-gray-700">
+        <div className="hidden lg:flex absolute z-10 bg-gray-900 w-full space-x-2 pb-2 mb-2 border-b border-gray-700">
           <div className="w-3 h-3 bg-red-500 rounded-full"></div>
           <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
           <div className="w-3 h-3 bg-green-500 rounded-full"></div>
         </div>
 
-        <div className="sm:hidden absolute z-10 self-center w-1/2 h-6 bg-black rounded-b-full top-0"/>
+        <div className="lg:hidden absolute z-10 self-center w-1/2 h-6 bg-black rounded-b-full top-0"/>
       </div>
       
       {/* Terminal content */}
-      <div className="overflow-auto z-0 p-2 mt-5 sm:mt-8 text-slate-400">
+      <div className="overflow-auto z-0 p-2 mt-5 lg:mt-8 text-slate-400">
         {children}
       </div>
     </ScrollArea>
