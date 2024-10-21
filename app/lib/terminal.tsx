3e417209036e984f7a0dae8b313d6b2eae813ef0
cnhhoang <cnhhoang.main@gmail.com>
2024-10-21 12:52:40 +1100

    Fix Terminal layout issues

--- app/lib/terminal.tsx
+++ app/lib/terminal.tsx
@@ -26,19 +26,18 @@ const Terminal: React.FC<TerminalProps> = ({ children, className = "", disabled 
       style={{ fontFamily: "monospace" }}
     >
       {/* Terminal header */}
-      <div className="z-10 flex flex-col">
-        <div className="hidden sm:flex absolute bg-gray-900 w-full space-x-2 pb-2 mb-2 border-b border-gray-700">
+      <div className="flex flex-col">
+        <div className="hidden sm:flex absolute z-10 bg-gray-900 w-full space-x-2 pb-2 mb-2 border-b border-gray-700">
           <div className="w-3 h-3 bg-red-500 rounded-full"></div>
           <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
           <div className="w-3 h-3 bg-green-500 rounded-full"></div>
         </div>
-        <div className="sm:hidden absolute self-center w-1/2 h-6 bg-black rounded-b-full top-0">
-          <div className="w-full "/>
-        </div>
+
+        <div className="sm:hidden absolute z-10 self-center w-1/2 h-6 bg-black rounded-b-full top-0"/>
       </div>
       
       {/* Terminal content */}
-      <div className="overflow-auto z-0 mt-10 text-slate-400">
+      <div className="overflow-auto z-0 p-2 mt-5 sm:mt-8 text-slate-400">
         {children}
       </div>
     </ScrollArea>
