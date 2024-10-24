95cb5591ff0a8ee60f725f0ca30c2e6600a89a90
cnhhoang <cnhhoang.main@gmail.com>
2024-10-24 21:51:14 +1100

    Update description for Algorithm and Data Structure project, and link to GitHub repo

--- app/routes/portfolio.cp-lib.cpp.tsx
+++ app/routes/portfolio.cp-lib.cpp.tsx
@@ -1,11 +1,29 @@
 // ====================================================================================================
 
+import { Link } from "@remix-run/react";
+import { Button } from "~/components/ui/button";
+
 //****************************************************************************************************
 export default function CPLibCPP()
 {
     return (
-        <div className="h-full w-full flex items-center justify-center">
-            CP Lib C++: Under development!
+        <div className="h-full w-full flex flex-col items-center gap-5 justify-between">
+            <div>
+                This project is an Algorithm and Data Structure Library optimized for competitive programming, written in C/C++, with a focus on optimal complexity. 
+            </div>
+            <div>
+                The library covers various topics from graph theory algorithms (Dijkstra, Kruskal, Bellman-Ford, Edmonds-Karp, Prim) to advanced data structures (2-3 Tree, Binary Indexed Tree, Segment Tree, RMQ).
+                Each algorithms are submitted to a variety of Online Judges (SPOJ, Codeforces, etc.) to ensure correctness and performance.
+            </div>
+            <Link to="https://github.com/cnhhoang/Data-Structures-and-Algorithms" asChild>
+                <div className={`border-green-500 text-green-500 px-2 py-1
+                                rounded-md border-2
+                                hover:rounded-full hover:font-bold
+                                ease-in-out
+                `}>
+                    Check it out
+                </div>
+            </Link>
         </div>
     );
 }
--- app/routes/portfolio.subleq.tsx
+++ app/routes/portfolio.subleq.tsx
@@ -4,8 +4,16 @@
 export default function Subleq()
 {
     return (
-        <div className="h-full w-full flex items-center justify-center">
-            SUBLEQ: Under development!
+        <div className="h-full w-full flex flex-col items-center justify-center">
+            <div>
+                SUBLEQ (Subtract and Branch if Less Than or Equal to Zero) is a type of One Instruction Set Computer (OISC), meaning it uses only one instruction to perform all operations. The SUBLEQ instruction subtracts the contents of one memory location from another and branches to a specified address if the result is less than or equal to zero. 
+            </div>
+            <div>
+                Despite its simplicity, SUBLEQ can be used to implement complex programs, including conditional structures like "if-then-else" and loops such as "for" and "while." This project demonstrates how minimalist computing concepts can still achieve practical programming logic.
+            </div>
+            <div className="text-emerald-500">
+                Source code to be added to GitHub.
+            </div>
         </div>
     );
 }
