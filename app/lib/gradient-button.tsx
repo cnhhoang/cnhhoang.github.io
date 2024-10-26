f60f35350a39946c7fa63914259d4a5ca8a73049
cnhhoang <cnhhoang.main@gmail.com>
2024-10-26 14:46:24 +1100

    Update gradient button

--- app/lib/gradient-button.tsx
+++ app/lib/gradient-button.tsx
@@ -1,8 +1,8 @@
 export default function GradientButton({ children }: { children: React.ReactNode })
 {
     return (
-        <div className={`bg-gradient-to-b from-sky-800 to-cyan-500  text-white
-            rounded-md sm:rounded-sm hover:rounded-full hover:font-bold
+        <div className={`bg-gradient-to-b from-blue-800 to-sky-500 text-white  hover:text-amber-300
+            rounded-md sm:rounded-sm hover:rounded-full font-bold
             flex items-center justify-center px-3 py-1 
         `}>
             {/* Give it a <Star className="ml-2"/> */}
