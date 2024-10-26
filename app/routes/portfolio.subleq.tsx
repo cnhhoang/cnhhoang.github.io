f0753f5f913679e04fd6201399c5de0adc6d77dd
cnhhoang <cnhhoang.main@gmail.com>
2024-10-26 15:32:55 +1100

    Update SUBLEQ description

--- app/routes/portfolio.subleq.tsx
+++ app/routes/portfolio.subleq.tsx
@@ -9,7 +9,7 @@ export default function Subleq()
     return (
         <div className="h-full w-full flex flex-col items-center justify-center">
             <div>
-                SUBLEQ (Subtract and Branch if Less Than or Equal to Zero) is a type of One Instruction Set Computer (OISC), meaning it uses only one instruction to perform all operations. The SUBLEQ instruction subtracts the contents of one memory location from another and branches to a specified address if the result is negative. 
+                SUBLEQ (Subtract and Branch if Less Than or Equal to Zero) is a special assembly language - a One Instruction Set Computer (OISC), meaning it uses only one instruction to perform all operations. The SUBLEQ instruction subtracts the contents of one memory location from another and branches to a specified address if the result is negative. 
             </div>
             <div>
                 Despite its simplicity, SUBLEQ can be used to implement complex programs, including conditional structures like "if-then-else" and loops such as "for" and "while." This project demonstrates how minimalist computing concepts can still achieve practical programming logic.
